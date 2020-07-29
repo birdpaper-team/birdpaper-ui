3c9ab810829ce52e6dbb403a44faa36d8ee36afc
sam <llh@birdpaper.com>
2020-07-29 11:24:14 +0800

    mod

--- packages/index.js
+++ packages/index.js
@@ -2,7 +2,7 @@
  * @Author: Sam
  * @Date: 2020-01-11 10:30:29
  * @Last Modified by: Sam
- * @Last Modified time: 2020-07-29 10:43:07
+ * @Last Modified time: 2020-07-29 11:23:56
  */
 import Vue from 'vue';
 // 导入组件
@@ -21,12 +21,9 @@ import bpAlert from './bp-alert';
 import bpItemBar from './bp-item-bar';
 import bpCheckbox from './bp-checkbox';
 import bpTable from './bp-table';
-<<<<<<< HEAD
 import bpRadio from './bp-radio';
 import bpSwitch from './bp-switch';
 import bpBadge from './bp-badge';
-=======
->>>>>>> c258ea3ff6bedbb1e08db76ed6a044a570485b45
 
 Vue.prototype.$message = msg;
 
@@ -43,12 +40,9 @@ const components = [
   bpCheckbox,
   bpAlert,
   bpTable,
-<<<<<<< HEAD
   bpRadio,
   bpSwitch,
   bpBadge,
-=======
->>>>>>> c258ea3ff6bedbb1e08db76ed6a044a570485b45
   bpRow,
   bpCol
 ];
