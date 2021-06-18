4ecac1518c83fbd18f980e06404a6f571be131aa
sam <llh@birdpaper.com>
2021-06-19 07:46:47 +0800

    v0.9.5

---
+++ packages/bp-buttongroup/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Sam
+ * @Date: 2021-06-12 19:04:02
+ * @Last Modified by:   Sam
+ * @Last Modified time: 2021-06-12 19:04:02
+ */
+
+import bpButtongroup from './src/bp-buttongroup.vue';
+
+bpButtongroup.install = function (Vue) {
+
+    Vue.component(bpButtongroup.name, bpButtongroup);
+
+}
+
+export default bpButtongroup;
---
+++ packages/bp-buttongroup/src/bp-buttongroup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="[round ? 'bp-buttongroup-round' : 'bp-buttongroup']">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bp-buttongroup",
+  props: {
+    round: { type: Boolean, default: false }, // 是否圆角
+  },
+};
+</script>
