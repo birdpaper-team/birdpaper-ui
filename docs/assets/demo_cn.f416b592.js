ad43d14b42dffb42c60285991c2800b3b9bc893e
Sam <llh@birdpaper.com>
2022-01-31 17:58:19 +0800

    7

--- docs/assets/demo_cn.f416b592.js
+++
@@ -1,7 +0,0 @@
-const t={},b=`<pre><code class="language-html">&lt;bp-button disabled type=&quot;default&quot;&gt;\u6B63\u5E38\u6309\u94AE&lt;/bp-button&gt;
-&lt;bp-button disabled type=&quot;primary&quot;&gt;\u4E3B\u8981\u6309\u94AE&lt;/bp-button&gt;
-&lt;bp-button disabled type=&quot;success&quot;&gt;\u6210\u529F\u6309\u94AE&lt;/bp-button&gt;
-&lt;bp-button disabled type=&quot;warning&quot;&gt;\u8B66\u544A\u6309\u94AE&lt;/bp-button&gt;
-&lt;bp-button disabled type=&quot;danger&quot;&gt;\u5371\u9669\u6309\u94AE&lt;/bp-button&gt;
-</code></pre>
-`;export{t as attributes,b as html};
