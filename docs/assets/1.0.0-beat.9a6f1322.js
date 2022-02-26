13bf97a10ff11065820902d9e670a65e8699389f
Sam <llh@birdpaper.com>
2022-02-26 17:42:13 +0800

    7.5

---
+++ docs/assets/1.0.0-beat.9a6f1322.js
@@ -0,0 +1,38 @@
+const e={},t=`<h2>v1.0.0 - beat. 7.5</h2>
+<p><code>2022-02-27</code></p>
+<ul>
+<li>Refactoring the <code>Input</code> structure and tail logic, and adding header element support</li>
+<li><code>Input</code> removes the border theme attribute</li>
+<li><code>Input</code> removes post-element slot content</li>
+<li><code>Input</code> adds component related methods</li>
+<li>Added the <code>show-password</code> attribute to the <code>Input</code> password Input box</li>
+<li>Fixed automatic height calculation of <code>Input</code> in multi-line text</li>
+<li>Fixed some errors in the documentation</li>
+</ul>
+<h2>v1.0.0-beat.7</h2>
+<p><code>2022-01-31</code></p>
+<ul>
+<li><code>Button</code> optimized DOM structure to reduce overall code components by 33%</li>
+<li><code>Button</code> added the dashed border bounding attribute</li>
+<li><code>Button</code> added html-type native type attribute</li>
+<li><code>Button</code> added min-width minimum width support</li>
+<li><code>Button</code> to attribute is renamed to href, and target controls the jump mode</li>
+<li><code>Button</code> component demo document optimization</li>
+</ul>
+<h2>v1.0.0-beat.6</h2>
+<p><code>2022-01-01</code></p>
+<ul>
+<li><code>Button</code> supports minimum width <code>min-width</code> setting</li>
+<li><code>Button</code> text Button supports<code>disabled</code>property</li>
+<li>some optimizations and bug fixes for <code>Pagination</code></li>
+<li>Optimized build style and component introduction code</li>
+<li>Support CDN introduction and update related documents</li>
+<li>Added baidu statistics script related to the official website</li>
+</ul>
+<h2>v1.0.0-beat.0</h2>
+<p><code>2021-12-12</code></p>
+<ul>
+<li>First public beta release</li>
+<li>Launch a new document site</li>
+</ul>
+`;export{e as attributes,t as html};
