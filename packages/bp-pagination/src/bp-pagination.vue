e85b428602e00a29904f014ce186c65b009dd093
Sam <llh@birdpaper.com>
2022-03-03 16:35:39 +0800

    pagination setup

--- packages/bp-pagination/src/bp-pagination.vue
+++ packages/bp-pagination/src/bp-pagination.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bp-pagination">
-    <div class="bp-pagination-total" v-if="showTotal">
+    <!--<div class="bp-pagination-total" v-if="showTotal">
       <p>
         共
         <span>
@@ -8,7 +8,7 @@
         </span>
         条
       </p>
-    </div>
+    </div>-->
 
     <bp-pagination-ul
       :disabled="disabled"
@@ -20,75 +20,63 @@
       @click="onPageClick"
     ></bp-pagination-ul>
 
-    <bp-select v-model="pageSize" :option-list="sizeOption"> </bp-select>
+    <!--<bp-select v-model="pageSize" :option-list="sizeOption"> </bp-select>-->
   </div>
 </template>
 
-<script>
-import { ref, watch } from "vue";
+<script setup>
+import { ref, watch, defineProps, defineEmits } from "vue";
 import bpPaginationUl from "./components/bp-pagination-ul.vue";
-export default {
-  name: "bp-pagination",
-  components: {
-    bpPaginationUl,
-  },
-  props: {
-    total: { type: [Number, String], default: 0 }, // 总条目数
-    pageSize: { type: [Number, String], default: () => 20 }, // 每页显示条数
-    current: { type: [Number, String], default: 1 }, // 当前页数，支持 v-model
-    disabled: { type: Boolean, default: false }, // 是否禁用
-    prevText: { type: String, default: "" }, // 替代图标显示的上一页文字
-    nextText: { type: String, default: "" }, // 替代图标显示的下一页文字
-    showTotal: { type: Boolean, default: false }, // 是否显示总数
-    sizesList: { type: Array, default: () => [10, 20, 50, 100] }, // 每页显示个数选择器的选项设置
 
-    // 后续支持
-    // size: { type: String, default: "normal" }, // 尺寸
-    // simple: { type: Boolean, default: false }, // 是否渲染成简洁版
-    // showElevator: { type: Boolean, default: false }, // 显示跳转电梯
-    // hideOnSinglePage: { type: Boolean, default: false }, // 显示跳转电梯
-    // showSizer: { type: Boolean, default: false }, // 显示分页，用来改变 page-size
-  },
-  emits: ["pageChange"],
-  setup(props, { emit }) {
-    const currentPage = ref(1); // 当前页
+const props = defineProps({
+  total: { type: [Number, String], default: 0 }, // 总条目数
+  pageSize: { type: [Number, String], default: () => 20 }, // 每页显示条数
+  current: { type: [Number, String], default: 1 }, // 当前页数，支持 v-model
+  disabled: { type: Boolean, default: false }, // 是否禁用
+  layout: { type: String, default: "" }, // 自定义分页布局
+  prevText: { type: String, default: "" }, // 替代图标显示的上一页文字
+  nextText: { type: String, default: "" }, // 替代图标显示的下一页文字
+  sizesList: { type: Array, default: () => [10, 20, 50, 100] }, // 每页显示个数选择器的选项设置
+  pagerCount: { type: Number, default: 7 }, //
+  background: { type: Boolean, default: false }, // 背景色支持
+  simple: { type: Boolean, default: false }, // 是否渲染成简洁版
+  hideOnSinglePage: { type: Boolean, default: false }, // 只有一页时隐藏
+});
+const emit = defineEmits(["pageChange"]);
+const currentPage = ref(1); // 当前页
 
-    // 点击页码切换
-    const onPageClick = (pageNum = 1) => {
-      currentPage.value = pageNum;
-      emit("pageChange", pageNum);
-    };
-
-    const sizeOption = ref([]);
-
-    watch(
-      () => props.current,
-      () => {
-        currentPage.value = Number(props.current);
-      },
-      {
-        immediate: true,
-      }
-    );
-    watch(
-      () => props.sizesList,
-      () => {
-        sizeOption.value = [];
-        for (let i = 0; i < props.sizesList.length; i++) {
-          const item = props.sizesList[i];
-          sizeOption.value.push({ label: `${item}条/页`, value: item });
-        }
-      },
-      {
-        immediate: true,
-      }
-    );
-
-    return {
-      sizeOption,
-      onPageClick,
-      currentPage,
-    };
-  },
+// 点击页码切换
+const onPageClick = (pageNum = 1) => {
+  currentPage.value = pageNum;
+  emit("pageChange", pageNum);
 };
+
+const sizeOption = ref([]);
+
+watch(
+  () => props.current,
+  () => {
+    currentPage.value = Number(props.current);
+  },
+  {
+    immediate: true,
+  }
+);
+watch(
+  () => props.sizesList,
+  () => {
+    sizeOption.value = [];
+    for (let i = 0; i < props.sizesList.length; i++) {
+      const item = props.sizesList[i];
+      sizeOption.value.push({ label: `${item}条/页`, value: item });
+    }
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+
+<script>
+export default { name: "bp-pagination" };
 </script>
