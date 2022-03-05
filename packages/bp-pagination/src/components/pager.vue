f174fd59aa42f7bcb225997fca29d387008a897a
Sam <llh@birdpaper.com>
2022-03-06 07:46:12 +0800

    total & totalPages

--- packages/bp-pagination/src/components/pager.vue
+++ packages/bp-pagination/src/components/pager.vue
@@ -1,14 +1,11 @@
 <template>
   <ul class="bp-pagination-container">
-    <li
-      :class="prevItemClass"
-      @click="onClick(isFirstPage ? '' : currentPage - 1)"
-    >
+    <li :class="prevItemClass" @click="onClick(isFirstPage ? '' : currentPage - 1)">
       <span v-if="prevText !== ''">{{ prevText }}</span>
       <i v-else class="ri-arrow-left-s-line"></i>
     </li>
 
-    <template v-for="(item, index) in pageList" :key="`page-item-${index}`">
+    <template v-for="(item, index) in pages" :key="`page-item-${index}`">
       <li
         :class="[
           'bp-pagination-item',
@@ -17,88 +14,53 @@
         ]"
         @click="onClick(item)"
       >
-        <span>{{ item }}</span>
+        <span>{{ item + 1 }}</span>
       </li>
     </template>
 
-    <li
-      :class="nextItemClass"
-      @click="onClick(isLastPage ? '' : currentPage + 1)"
-    >
+    <li :class="nextItemClass" @click="onClick(isLastPage ? '' : currentPage + 1)">
       <span v-if="nextText !== ''">{{ nextText }}</span>
       <i v-else class="ri-arrow-right-s-line"></i>
     </li>
   </ul>
 </template>
 
-<script>
-import { computed, ref, watch } from "vue";
-export default {
-  name: "bp-pagination-ul",
-  props: {
-    currentPage: { type: [Number,String], default: 1 }, // 当前激活页
-    total: { type: [Number,String], default: 0 }, // 总条目数
-    pageSize: { type: [Number,String], default: () => 10 }, // 每页条数
-    disabled: { type: Boolean, default: false }, // 是否禁用
-    prevText: { type: String, default: "" }, // 替代图标显示的上一页文字
-    nextText: { type: String, default: "" }, // 替代图标显示的下一页文字
-  },
-  emits: ["click"],
-  setup(props, { emit }) {
-    const pageList = ref([]); // 模板所渲染的分页数据
-    const pageCount = ref(1); // 总页数
+<script setup>
+import { computed, ref, watch, defineProps } from "vue";
 
-    const isFirstPage = computed(() => props.currentPage === 1);
-    const isLastPage = computed(() => props.currentPage === pageCount.value);
+const props = defineProps({
+  currentPage: { type: [Number, String], default: 1 }, // 当前激活页
+  pages: { type: [Number, String], default: 0 },
+  disabled: { type: Boolean, default: false }, // 是否禁用
+  prevText: { type: String, default: "" }, // 替代图标显示的上一页文字
+  nextText: { type: String, default: "" }, // 替代图标显示的下一页文字
+});
+const emit = defineEmits(["click"]);
 
-    const prevItemClass = computed(() => {
-      let name = ["bp-pagination-item", "bp-prev-page"];
-      if (isFirstPage.value) name.push("pagination-item-disabled");
-      return name;
-    });
+const pageCount = ref(1); // 总页数
 
-    const nextItemClass = computed(() => {
-      let name = ["bp-pagination-item", "bp-next-page"];
-      if (isLastPage.value) name.push("pagination-item-disabled");
-      return name;
-    });
+const isFirstPage = computed(() => props.currentPage === 1);
+const isLastPage = computed(() => props.currentPage === pageCount.value);
 
-    const onClick = (pageNum = "") => {
-      if (props.disabled || pageNum === "") return;
-      emit("click", pageNum);
-    };
+const prevItemClass = computed(() => {
+  let name = ["bp-pagination-item", "bp-prev-page"];
+  if (isFirstPage.value) name.push("pagination-item-disabled");
+  return name;
+});
 
-    function setPageList() {
-      pageList.value = [];
-      const { total, pageSize } = props;
+const nextItemClass = computed(() => {
+  let name = ["bp-pagination-item", "bp-next-page"];
+  if (isLastPage.value) name.push("pagination-item-disabled");
+  return name;
+});
 
-      const len = Math.ceil(total / pageSize);
-      pageCount.value = len;
-
-      for (let i = 0; i < len; i++) {
-        pageList.value.push(i + 1);
-      }
-    }
-
-    watch(
-      () => props.total,
-      () => {
-        setPageList();
-      },
-      {
-        immediate: true,
-      }
-    );
-
-    return {
-      onClick,
-      isFirstPage,
-      isLastPage,
-      pageList,
-      pageCount,
-      prevItemClass,
-      nextItemClass,
-    };
-  },
+const onClick = (pageNum = "") => {
+  if (props.disabled || pageNum === "") return;
+  emit("click", pageNum);
 };
+
+</script>
+
+<script>
+export default { name: "bp-pagination-pager" };
 </script>
--- packages/bp-pagination/src/pagination.js
+++ packages/bp-pagination/src/pagination.js
@@ -4,15 +4,20 @@
  * @LastModifiedBy:   Sam
  * @LastModifiedTime: 2022-03-04 06:31:44
  */
-import { ref, computed } from "vue";
+import { ref, computed, watchEffect } from "vue";
+import total from "./components/total.vue";
+import totalPages from "./components/totalPages.vue";
 import pager from "./components/pager.vue";
 
-export const usePagination = (props,emit) => {
+export const usePagination = (props, emit) => {
   const LAYOUT_MAP = {
-    pager
-  }
+    pager,
+    total,
+    totalPages
+  };
 
   const currentPage = ref(1); // 当前页
+  const totalPagesNum = ref(0); // 页数
 
   // 点击页码切换
   const onPageClick = (pageNum = 1) => {
@@ -20,23 +25,41 @@ export const usePagination = (props,emit) => {
     emit("pageChange", pageNum);
   };
 
+  watchEffect(() => {
+    const { total, pageSize } = props;
+    totalPagesNum.value = Math.ceil(total / pageSize);
+  })
+
   // 布局组件，返回一个包含组件模板、属性以及事件的 Array
   const layoutsComponents = computed(() => {
-    const LAYOUT_LIST = props.layout.split(','); // 可自定义的分页布局配置
+    const LAYOUT_LIST = props.layout.split(","); // 可自定义的分页布局配置
 
     // 各组件的属性以及事件配置
-    const COMPONENTS_LIST = [], COMPONENTS_CFG = {
-      'pager': {
-        bind: {
-          total: props.total,
-          pageSize: props.pageSize,
-          prevText: props.prevText,
-          nextText: props.nextText,
-          currentPage: currentPage.value,
+    const COMPONENTS_LIST = [],
+      COMPONENTS_CFG = {
+        pager: {
+          bind: {
+            pages: totalPagesNum.value,
+            prevText: props.prevText,
+            nextText: props.nextText,
+            currentPage: currentPage.value,
+          },
+          event: "click",
+          eventName: onPageClick,
         },
-        event: "click", eventName: onPageClick
-      }
-    };
+        total: {
+          bind: {
+            value: props.total,
+            tmpString: props.totalTmpString
+          },
+        },
+        totalPages: {
+          bind: {
+            value: totalPagesNum.value,
+            tmpString: props.pagesTmpString
+          },
+        },
+      };
 
     LAYOUT_LIST.map((name) => {
       COMPONENTS_LIST.push({ name, component: LAYOUT_MAP[name], ...COMPONENTS_CFG[name] });
@@ -48,5 +71,5 @@ export const usePagination = (props,emit) => {
   return {
     layoutsComponents,
     currentPage,
-  }
-}
+  };
+};
