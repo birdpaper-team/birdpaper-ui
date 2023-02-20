bcc94d420d34d072ed60a5e74d033039a615b363
Sam <llh@birdpaper.com>
2023-02-20 16:06:18 +0800

    Feat(docs): 代码演示组件样式

--- docs/components/demo-block/src/index.vue
+++ docs/components/demo-block/src/index.vue
@@ -2,25 +2,33 @@
  * @ Author: Sam
  * @ Create Time: 2023-02-18 14:57:46
  * @ Modified by: Sam
- * @ Modified time: 2023-02-19 17:00:41
+ * @ Modified time: 2023-02-20 15:57:43
  * @ Description: 代码演示组件
  -->
 
 <template>
   <div class="demo-block">
+    <!-- 代码演示 -->
     <div class="demo-block-area">
       <component v-if="cm" :is="cm"></component>
     </div>
+
+    <!-- 操作区域 -->
     <div class="demo-block-footer">
-      <p>展开</p>
+      <div :class="['icon-item', { active: showCode }]" @click="showCode = !showCode">
+        <code-icon theme="filled" size="14" :fill="showCode ? '#434343' : '#8c8c8c'" />
+      </div>
     </div>
-    <div class="demo-block-source">
+
+    <!-- 源码 -->
+    <div :class="['demo-block-source', { 'source-open': showCode }]">
       <div class="demo-block-source-inner" v-html="decodeURIComponent(codeString)"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts" name="demo-block">
+import { Code as codeIcon } from "@icon-park/vue-next";
 import { defineAsyncComponent, markRaw, ref } from "vue";
 
 interface Prop {
@@ -36,6 +44,7 @@ const props = withDefaults(defineProps<Prop>(), {
 });
 
 const cm = ref();
+const showCode = ref<boolean>(false);
 
 const glob = import.meta.glob(`../../../src/example/**/*.vue`);
 
@@ -43,5 +52,7 @@ cm.value = markRaw(defineAsyncComponent(glob[`../../../src/${props.src}.vue`] as
 </script>
 
 <style lang="less" scoped>
-@import "./index.less";
+:deep(.min-light) {
+  background: none !important;
+}
 </style>
--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -2,13 +2,17 @@
 import DefaultTheme from "vitepress/theme";
 import "./var.less";
 import "./index.less";
+import "./font.less";
+import "../../../components/style/index.less";
 
 import NoFound from "./NotFound.vue";
 import { EnhanceAppContext } from "vitepress";
 
 import DemoBlock from "../../../components/demo-block";
-import BirdpaperUI from "birdpaper-ui"
-import "birdpaper-ui/style/index.less"
+import BirdpaperUI from "birdpaper-ui";
+import "birdpaper-ui/style/index.less";
+
+import "@icon-park/vue-next/styles/index.css";
 
 export default {
   ...DefaultTheme,
