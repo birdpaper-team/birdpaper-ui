e62abac76032ca4d078cb9894575f0d8ac6f9926
Sam <llh@birdpaper.com>
2023-02-21 20:26:08 +0800

    Feat(docs): 完善表格类型

--- docs/src/types/tableBlock.ts
+++ docs/src/types/tableBlock.ts
@@ -16,3 +16,41 @@ interface PropsTableItem {
   /** 版本号 */
   version?: string;
 }
+
+/** Event 事件 */
+interface EventTableItem {
+  /** 事件名 */
+  name: string;
+  /** 说明 */
+  remark: string;
+  /** 参数 */
+  params?: string;
+  /** 版本号 */
+  version?: string;
+}
+
+/** Method 方法 */
+interface MethodTableItem {
+  /** 方法名 */
+  name: string;
+  /** 说明 */
+  remark: string;
+  /** 参数 */
+  params?: string;
+  /** 返回值 */
+  returns?: string;
+  /** 版本号 */
+  version?: string;
+}
+
+/** Slot 插槽 */
+interface SlotTableItem {
+  /** 方法名 */
+  name: string;
+  /** 说明 */
+  remark: string;
+  /** 参数 */
+  params?: string;
+  /** 版本号 */
+  version?: string;
+}
