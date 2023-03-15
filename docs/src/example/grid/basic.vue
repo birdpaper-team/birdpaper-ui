a7a3aa67ef4334dc47bff60ef4a531011069db36
Sam <llh@birdpaper.com>
2023-03-15 17:15:59 +0800

    Feat(packages): col 栏位和偏移

--- docs/src/example/grid/basic.vue
+++ docs/src/example/grid/basic.vue
@@ -1,5 +1,29 @@
 <template>
-  <bp-row>
-    <bp-col></bp-col>
-  </bp-row>
+  <div class="demo-row">
+    <bp-row>
+      <bp-col span="4" class="demo"></bp-col>
+      <bp-col span="6" class="demo"></bp-col>
+      <bp-col span="6" class="demo"></bp-col>
+      <bp-col span="8" class="demo"></bp-col>
+    </bp-row>
+  </div>
+  <div class="demo-row">
+    <bp-row>
+      <bp-col span="6" offset="2" class="demo"></bp-col>
+      <bp-col span="8" offset="3" class="demo"></bp-col>
+    </bp-row>
+  </div>
 </template>
+
+<style lang="less" scoped>
+.demo-row {
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+  .demo {
+    border-radius: 4px;
+    background-color: #449efb;
+    height: 30px;
+  }
+}
+</style>
--- packages/components/grid/src/col.vue
+++ packages/components/grid/src/col.vue
@@ -5,14 +5,19 @@
 </template>
 
 <script setup lang="ts" name="col">
-import { computed } from 'vue';
+import { computed } from "vue";
 
 const props = defineProps({
-
+  span: { type: [String, Number], default: "" },
+  offset: { type: [String, Number], default: "" },
 });
 
 const name = "col";
 const cls = computed(() => {
-  return [`bp-${name}`];
+  let className = [`bp-${name}`];
+  Number(props.span) !== 0 ? className.push(`bp-${name}-${props.span}`) : "";
+  Number(props.offset) !== 0 ? className.push(`bp-${name}-offset-${props.offset}`) : "";
+
+  return className;
 });
 </script>
