a31424c784eeab28bb63e2b3961831b931afe646
Sam <llh@birdpaper.com>
2023-03-20 17:34:47 +0800

    Feat(docs): 初次构建

---
+++ docs/src/.vitepress/dist/assets/chunks/events.37783c4c.js
@@ -0,0 +1,1 @@
+const a=[{name:"click",remark:"点击触发",params:"none"}];export{a as default};
---
+++ docs/src/.vitepress/dist/assets/chunks/props.c5bf94ab.js
@@ -0,0 +1,1 @@
+const a=[{name:"size",remark:"间距大小",type:["Enum","Number"],optional:["mini","small","normal","large"],default:"normal"}];export{a as default};
