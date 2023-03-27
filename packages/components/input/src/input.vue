790d631edc146f705fe51b6b27f130a95a457766
sam <llh@birdpaper.com>
2023-03-28 05:02:13 +0800

    Feat(packages): input

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="bp-input">
-    <input type="text" class="bp-input-inner">
+    <input type="text" class="bp-input-inner" />
   </div>
 </template>
 
 <script setup lang="ts" name="input">
-console.log('[  ]-6', "input");
+import { PropType } from "vue";
+import { InputSize } from "./types";
+
+const props = defineProps({
+  /** 输入框尺寸 Size of the input */
+  size: { type: String as PropType<InputSize>, default: "normal" },
+  /** 是否禁用 Disabled or not */
+  disabled: { type: Boolean, default: false },
+  /** 是否只读状态 Readonly or not */
+  readonly: { type: Boolean, default: false },
+  /** 是否错误状态 Error or not */
+  isError: { type: Boolean, default: false },
+});
 </script>
---
+++ packages/components/input/src/types.ts
@@ -0,0 +1,2 @@
+/** 输入框尺寸，迷你、小型、普通、大型 */
+export type InputSize = "mini" | "small" | "normal" | "large";
