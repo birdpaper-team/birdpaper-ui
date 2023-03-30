723be36b024ed33c1aa9a0217c0941f4affae517
Sam <llh@birdpaper.com>
2023-03-30 08:17:10 +0800

    Feat(packages): input

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="inpClass">
-    <input type="text" class="bp-input-inner" />
+    <input type="text" :disabled="disabled" :readonly="readonly" class="bp-input-inner" :placeholder="placeholder" />
   </div>
 </template>
 
@@ -18,9 +18,13 @@ const props = defineProps({
   readonly: { type: Boolean, default: false },
   /** 是否错误状态 Error or not */
   isError: { type: Boolean, default: false },
+  /** 占位提示文字 The placeholder text*/
+  placeholder: { type: String, default: "" },
 });
 
 const inpClass = computed(() => {
-  return name;
+  const status =
+    (props.disabled && `disabled`) || (props.readonly && `readonly`) || (props.isError && `error`) || "normal";
+  return [name, `${name}-size-${props.size}`, `${name}-status-${status}`];
 });
 </script>
