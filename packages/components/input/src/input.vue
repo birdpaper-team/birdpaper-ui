afc887be31bacc35e31d30eb8f987d5cc2c56be6
Sam <llh@birdpaper.com>
2023-03-31 08:15:34 +0800

    Feat(packages): input

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -10,21 +10,38 @@ import { InputSize } from "./types";
 const name = "bp-input";
 
 const props = defineProps({
+  /** 绑定值 Binding value */
+  modelValue: { type: String, default: "" },
   /** 输入框尺寸 Size of the input */
   size: { type: String as PropType<InputSize>, default: "normal" },
   /** 是否禁用 Disabled or not */
   disabled: { type: Boolean, default: false },
   /** 是否只读状态 Readonly or not */
   readonly: { type: Boolean, default: false },
-  /** 是否错误状态 Error or not */
-  isError: { type: Boolean, default: false },
+  /** 是否警示状态 Danger or not */
+  isDanger: { type: Boolean, default: false },
   /** 占位提示文字 The placeholder text*/
   placeholder: { type: String, default: "" },
+  /** 限制输入最大长度 Restricts the maximum input length */
+  maxLength: { type: Number, default: 0 },
+  /** 是否展示字数限制提示 Display word limit prompts or not */
+  showLimit: { type: Boolean, default: false },
 });
 
+const emits = defineEmits<{
+  (e: "update:modelValue", data: string): void;
+  (e: "focus"): void;
+  (e: "keyup"): void;
+  (e: "blur"): void;
+  (e: "clear"): void;
+}>();
+
 const inpClass = computed(() => {
-  const status =
-    (props.disabled && `disabled`) || (props.readonly && `readonly`) || (props.isError && `error`) || "normal";
+  const status = getStatus();
   return [name, `${name}-size-${props.size}`, `${name}-status-${status}`];
 });
+
+function getStatus() {
+  return (props.disabled && "disabled") || (props.readonly && "readonly") || (props.isDanger && "danger") || "normal";
+}
 </script>
