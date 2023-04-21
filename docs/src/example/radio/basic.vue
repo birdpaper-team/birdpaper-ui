0388ed3259c4855585777e2e166ac25b6f8fb44e
Sam <llh@birdpaper.com>
2023-04-21 17:36:47 +0800

    Feat(packages): radio

--- docs/src/example/radio/basic.vue
+++ docs/src/example/radio/basic.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <bp-radio></bp-radio>
+    <bp-radio v-model="val" :value="true">选项一</bp-radio>
+    <bp-button @click="val = !val">测试</bp-button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const val = ref(true);
+</script>
--- packages/components/radio/src/radio.vue
+++ packages/components/radio/src/radio.vue
@@ -1,12 +1,33 @@
 <template>
-  <div></div>
+  <div :class="name" @click="handleInput">
+    <input type="radio" :class="`${name}-inner`" />
+
+    <span :class="[`${name}-radio`, isCheck ? `${name}-check` : '']"></span>
+    <span :class="`${name}-label`">
+      <slot></slot>
+    </span>
+  </div>
 </template>
 
 <script setup lang="ts" name="Radio">
+import { computed } from "vue";
+
 const props = defineProps({
   modelValue: { type: [String, Number, Boolean], default: false },
   value: { type: [String, Number, Boolean], default: false },
   disabled: { type: Boolean, default: false },
 });
+const emits = defineEmits<{
+  (e: "update:modelValue", val: string | number | boolean): void;
+}>();
+
 const name = "bp-radio";
+
+const handleInput = () => {
+  emits("update:modelValue", props.value);
+};
+
+const isCheck = computed(() => {
+  return props.modelValue === props.value;
+});
 </script>
