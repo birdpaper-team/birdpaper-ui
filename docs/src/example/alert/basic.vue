91c6ac4a49ae215cec92ad93b67f1dd82c6fd98b
Sam <llh@birdpaper.com>
2023-04-24 16:59:43 +0800

    Feat(packages): space \&\& alert doscs

--- docs/src/example/alert/basic.vue
+++ docs/src/example/alert/basic.vue
@@ -1,3 +1,8 @@
 <template>
-  <div></div>
+  <bp-space type="horizontal" size="mini">
+    <bp-alert>提示内容</bp-alert>
+    <bp-alert type="warning" closeable>提示内容</bp-alert>
+    <bp-alert type="success">提示内容</bp-alert>
+    <bp-alert type="error">提示内容</bp-alert>
+  </bp-space>
 </template>
