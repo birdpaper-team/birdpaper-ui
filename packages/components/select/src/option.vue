622f2672a420a8c85c28995d9892d318caeb1d80
Sam <llh@birdpaper.com>
2023-05-01 20:50:12 +0800

    Feat(packages): option

--- packages/components/select/src/option.vue
+++ packages/components/select/src/option.vue
@@ -3,4 +3,8 @@
 </template>
 
 <script setup lang="ts" name="Option">
+const props = defineProps({
+  value: { type: [String, Number], default: "" },
+  label: { type: [String, Number], default: "" },
+});
 </script>
