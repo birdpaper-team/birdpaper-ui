a4ed5aa1c08d631a93ddd96bf9685f6e14602d1d
Sam <llh@birdpaper.com>
2023-05-16 08:12:25 +0800

    Feat(packages): switch

--- packages/components/switch/src/switch.vue
+++ packages/components/switch/src/switch.vue
@@ -1,11 +1,36 @@
 <template>
-  <div></div>
+   <div :class="cls" @click="handleClick">
+    <input type="checkbox" :class="`${name}-inner`" />
+
+    <span :class="`${name}-label`"></span>
+  </div>
 </template>
 
 <script setup lang="ts" name="Switch">
+import { computed } from "vue";
+
 const props = defineProps({
+  /** 绑定值 Binding value */
   modelValue: { type: Boolean, default: false },
+  /** 是否禁用 Disabled or not */
+  disabled: { type: Boolean, default: false },
 });
+const emits = defineEmits<{
+  "update:modelValue": [value: boolean];
+}>();
 
 const name = "bp-switch";
+const cls = computed(() => {
+  let clsName = [name];
+  if (props.disabled) {
+    clsName.push(`${name}-disabled`);
+  }
+  return clsName;
+});
+
+const isCheck = computed(() => props.modelValue);
+
+const handleClick = () => {
+  emits("update:modelValue", !props.modelValue);
+};
 </script>
