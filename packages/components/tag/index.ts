3d7667d1c97810ffcf59d353ffa25c5733f54b5c
Sam <llh@birdpaper.com>
2023-05-18 15:17:13 +0800

    Feat(packages): tag

---
+++ packages/components/tag/index.ts
@@ -0,0 +1,14 @@
+import { App } from "vue";
+import _tag from "./src/tag.vue";
+import { getComponentsPrefix } from "../../utils/config";
+
+_tag.name = getComponentsPrefix() + _tag.name;
+
+const Tag = Object.assign(_tag, {
+  install: (app: App) => {
+    app.component(_tag.name, _tag);
+  },
+});
+
+export type tagInstance = InstanceType<typeof _tag>;
+export default Tag;
---
+++ packages/components/tag/src/tag.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="cls" v-if="visible">
+    <span :class="`${name}-inner`">
+      <slot></slot>
+    </span>
+
+    <i v-if="closeable" class="ri-close-line" @click="handleClose"></i>
+  </div>
+</template>
+
+<script setup lang="ts" name="Tag">
+import { computed, ref } from "vue";
+
+const props = defineProps({
+  status: { type: String, default: "normal" },
+  closeable: { type: Boolean, default: false },
+});
+const name = "bp-tag";
+
+const cls = computed(() => {
+  return [name, `${name}-${props.status}`];
+});
+
+const visible = ref(true);
+const handleClose = () => {
+  if (!props.closeable) return;
+
+  visible.value = false;
+};
+</script>
