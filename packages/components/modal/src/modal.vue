ba1271828d6b567ad412dfaa6ddaa24662b3f5ba
Sam <llh@birdpaper.com>
2023-05-21 04:15:54 +0800

    Feat(packages): modal

--- packages/components/modal/src/modal.vue
+++ packages/components/modal/src/modal.vue
@@ -1,25 +1,29 @@
 <template>
   <teleport to="body">
-    <div :class="`${name}-mask`" v-show="visible"></div>
+    <Transition name="fade-modal-mask">
+      <div :class="`${name}-mask`" v-show="visible"></div>
+    </Transition>
 
-    <div :class="`${name}-wrapper`" v-show="visible">
-      <div :class="`${name}`">
-        <div :class="`${name}-header`">
-          <p :class="`${name}-header-title`">{{ title }}</p>
-          <i :class="`${name}-header-close`" class="ri-close-line" @click="handleCancle"></i>
-        </div>
-        <div :class="`${name}-body`">
-          <slot></slot>
-        </div>
-        <div :class="`${name}-footer`">
-          <bp-space justify="flex-end">
-            <bp-button @cilik="handleCancle">取消</bp-button>
-            <bp-button @click="handleOk" type="primary" status="primary">确认</bp-button>
-          </bp-space>
-          <slot name="footer"></slot>
+    <Transition name="modal-fade">
+      <div :class="`${name}-wrapper`" v-show="visible">
+        <div :class="`${name}`">
+          <div :class="`${name}-header`">
+            <p :class="`${name}-header-title`">{{ title }}</p>
+            <i :class="`${name}-header-close`" class="ri-close-line" @click="handleCancle"></i>
+          </div>
+          <div :class="`${name}-body`">
+            <slot></slot>
+          </div>
+          <div :class="`${name}-footer`">
+            <bp-space justify="flex-end">
+              <bp-button @click="handleCancle">取消</bp-button>
+              <bp-button @click="handleOk" type="primary" status="primary">确认</bp-button>
+            </bp-space>
+            <slot name="footer"></slot>
+          </div>
         </div>
       </div>
-    </div>
+    </Transition>
   </teleport>
 </template>
 
