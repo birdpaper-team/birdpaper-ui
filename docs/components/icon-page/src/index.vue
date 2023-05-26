0b738cb67e02765ba74e021b58d3ed6532e26467
Sam <llh@birdpaper.com>
2023-05-27 06:15:35 +0800

    Feat(packages): icon\message

--- docs/components/icon-page/src/index.vue
+++ docs/components/icon-page/src/index.vue
@@ -2,7 +2,7 @@
   <div class="icon-page">
     <div class="icon-page-inner">
       <template v-for="v in iconList">
-        <div class="icon-item">
+        <div class="icon-item" @click="handleCopy(v)">
           <i :class="v"></i>
           <span v-text="v"></span>
         </div>
@@ -12,5 +12,18 @@
 </template>
 
 <script setup lang="ts" name="icon-page">
+import useClipboard from "vue-clipboard3";
+import { Message } from "birdpaper-ui";
 import iconList from "./icon";
+
+/** 复制到剪贴板 */
+const { toClipboard } = useClipboard();
+const handleCopy = async (item: any) => {
+  try {
+    await toClipboard(item);
+    Message.success(`已复制到剪贴板: ${item}`);
+  } catch (err) {
+    Message.error((err as Error).message);
+  }
+};
 </script>
