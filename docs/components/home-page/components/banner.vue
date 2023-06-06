47241f46bea385b4e6f0bd4ece32a28bef90797e
Sam <llh@birdpaper.com>
2023-06-06 17:02:48 +0800

    Docs(packages): brade

--- docs/components/home-page/components/banner.vue
+++ docs/components/home-page/components/banner.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home-page-banner">
     <div class="left">
-      <p class="app-name">{{ _data.name }}</p>
+      <div class="app-name">
+        {{ _data.name }}
+        <!-- <div class="brage-version">
+          <div class="brage-version-inner">
+            {{ _data.version }}
+          </div>
+        </div> -->
+      </div>
       <p class="app-intro" v-html="_data.intro_cn + _data.intro_en"></p>
       <div class="btn-area">
         <bp-space>
@@ -27,5 +34,6 @@ const _data = {
     start: "快速上手",
     git: "在 Github 上查看",
   },
+  version: "v2.0.0",
 };
 </script>
