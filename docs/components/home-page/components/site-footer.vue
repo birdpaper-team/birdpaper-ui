641e6d27b16be3f0108a145b0b88a76c884f43f3
Sam <llh@birdpaper.com>
2023-07-08 09:04:08 +0800

    Feat(docs): 公众号二维码

--- docs/components/home-page/components/site-footer.vue
+++ docs/components/home-page/components/site-footer.vue
@@ -2,34 +2,25 @@
   <div :class="name">
     <div :class="`${name}-container`">
       <div :class="`${name}-menu`">
-        <div :class="`${name}-menu-group`">
+        <div :class="`${name}-menu-group`" v-for="item in footerRouteList">
           <div :class="`${name}-menu-group-title`">
-            <p>设计</p>
+            <p>{{ item.title }}</p>
           </div>
           <div :class="`${name}-menu-group-list`">
             <ul>
-              <li :class="`${name}-menu-group-list-item`">设计指引</li>
-              <li :class="`${name}-menu-group-list-item`">设计资源</li>
-            </ul>
-          </div>
-        </div>
-        <div :class="`${name}-menu-group`">
-          <div :class="`${name}-menu-group-title`">
-            <p>设计</p>
-          </div>
-          <div :class="`${name}-menu-group-list`">
-            <ul>
-              <li :class="`${name}-menu-group-list-item`">设计指引</li>
-              <li :class="`${name}-menu-group-list-item`">设计资源</li>
-              <li :class="`${name}-menu-group-list-item`">设计资源</li>
-              <li :class="`${name}-menu-group-list-item`">设计资源</li>
-              <li :class="`${name}-menu-group-list-item`">设计资源</li>
+              <li :class="`${name}-menu-group-list-item`" v-for="v in item.children">
+                <bp-link status="normal">{{ v.title }}</bp-link>
+              </li>
             </ul>
           </div>
         </div>
       </div>
       <div :class="`${name}-mp`">
-        <div :class="`${name}-mp-inner`"></div>
+        <div :class="`${name}-mp-inner`">
+          <img :src="qrcodeMp" alt="扫码关注" />
+          <p :class="`${name}-mp-inner-title`">联系我们</p>
+          <p :class="`${name}-mp-inner-intro`">扫码关注公众号-小纸鹤</p>
+        </div>
       </div>
     </div>
     <div :class="`${name}-info`">
@@ -49,5 +40,41 @@
 </template>
 
 <script setup lang="ts">
+import qrcodeMp from "../../../src/public/qrcode_mp.jpg";
+
 const name = "site-footer";
+
+interface FooterRouteItem {
+  title: string;
+  children: {
+    title: string;
+    path: string;
+  }[];
+}
+
+const footerRouteList: FooterRouteItem[] = [
+  {
+    title: "设计",
+    children: [
+      { title: "价值观", path: "#" },
+      { title: "设计原则", path: "#" },
+    ],
+  },
+  {
+    title: "开发指南",
+    children: [
+      { title: "安装", path: "#" },
+      { title: "快速上手", path: "#" },
+      { title: "更新日志", path: "#" },
+    ],
+  },
+  {
+    title: "社区",
+    children: [
+      { title: "Github", path: "#" },
+      { title: "Moya", path: "#" },
+      { title: "语雀", path: "#" },
+    ],
+  },
+];
 </script>
