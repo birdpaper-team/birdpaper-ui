4f74a1d2e28bc02a40f7b28a38e3fb2d11dc4801
Sam <llh@birdpaper.com>
2023-07-25 09:52:08 +0800

    Feat(packages): image

--- docs/src/example/image/basic.vue
+++ docs/src/example/image/basic.vue
@@ -1,3 +1,3 @@
 <template>
-  <!-- <bp-image src="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a8c8cdb109cb051163646151a4a5083b.png~tplv-uwbnlip3yd-webp.webp"></bp-image> -->
+  <bp-image width="120" src="https://cos.birdpaper.com/birdpaper-ui/assets/image-demo/image6.jpg"></bp-image>
 </template>
--- packages/components/image/src/image.vue
+++ packages/components/image/src/image.vue
@@ -1,11 +1,21 @@
 <template>
-  <div :class="name">
-    <img ref="imageRef" />
+  <div :class="name" :style="imgStyle">
+    <img ref="imageRef" :style="{ ...imgStyle, ...fitStyle }" @load="onLoad" @error="onError" />
+    <slot name="loading" v-if="isLoading">
+      <div :class="`${name}-loading`">
+        <span>加载中</span>
+      </div>
+    </slot>
+    <slot name="error" v-if="!isLoading && loadStatus === 'error'">
+      <div :class="`${name}-error`">
+        <i class="ri-image-2-line"></i>
+      </div>
+    </slot>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watchEffect } from "vue";
+import { CSSProperties, PropType, computed, defineComponent, ref, watchEffect } from "vue";
 
 export default defineComponent({
   name: "Image",
@@ -13,24 +23,53 @@ export default defineComponent({
     /** 图片资源地址 */
     src: { type: String, default: "" },
     /** 图片适应类型 */
-    fit: { type: String, default: "" },
+    fit: { type: String as PropType<"contain" | "cover" | "fill" | "none" | "scale-down">, default: "fill" },
+    alt: { type: String, default: "" },
+    width: { type: [String, Number] as PropType<string | number> },
+    height: { type: [String, Number] as PropType<string | number> },
   },
   emits: ["load", "error"],
   setup(props, { emit }) {
     const name = "bp-image";
     const imageRef = ref();
-    const loading = ref<boolean>(false);
-    const isLoadError = ref<boolean>(false);
+    const loadStatus = ref<"loading" | "load" | "error">("loading");
+
+    const isLoading = computed<boolean>(() => loadStatus.value === "loading");
+    const imgStyle = computed<CSSProperties>(() => ({
+      width: `${props.width}px`,
+      height: `${props.height}px`,
+    }));
+    const fitStyle = computed<CSSProperties>(() => {
+      if (props.fit) {
+        return { objectFit: props.fit };
+      }
+      return {};
+    });
+
+    const onLoad = () => {
+      loadStatus.value = "load";
+      emit("load");
+    };
+    const onError = () => {
+      loadStatus.value = "error";
+      emit("error");
+    };
 
     watchEffect(() => {
       if (!props.src || !imageRef.value) return;
 
-      // imageRef.value.src = props.src;
+      imageRef.value.src = props.src;
     });
 
     return {
       name,
       imageRef,
+      loadStatus,
+      onLoad,
+      onError,
+      imgStyle,
+      fitStyle,
+      isLoading,
     };
   },
 });
