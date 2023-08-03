66d8e59b00345589efbc143257e1a13f68bd96a1
Sam <llh@birdpaper.com>
2023-08-03 10:41:01 +0800

    Feat(packages): table empty

--- packages/components/table/src/components/empty.vue
+++ packages/components/table/src/components/empty.vue
@@ -1,6 +1,11 @@
 <template>
   <tr class="bp-table-empty-tr">
-    <td :colspan="colspan">暂无数据</td>
+    <td :colspan="colspan">
+      <div class="bp-table-empty-tr-inner">
+        <i class="ri-inbox-2-line"></i>
+        <span>暂无数据</span>
+      </div>
+    </td>
   </tr>
 </template>
 
