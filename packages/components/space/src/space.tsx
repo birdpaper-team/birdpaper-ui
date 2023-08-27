e17cf9e2faf6b0e25f768930244df97016a9853b
Sam <llh@birdpaper.com>
2023-08-27 08:42:03 +0800

    Feat(packages): space 支持垂直对齐方式和分割内容

--- packages/components/space/src/space.tsx
+++ packages/components/space/src/space.tsx
@@ -6,9 +6,14 @@ import { SizeType, SpaceType } from "./types";
 export default defineComponent({
   name: "Space",
   props: {
+    /** 间距大小 */
     size: { type: [Number, String] as PropType<SizeType>, default: "normal" },
+    /** 布局类型 */
     type: { type: String as PropType<SpaceType>, default: "horizontal" },
+    /** 水平对齐方式 */
     justify: { type: String, default: "flex-start" },
+    /** 垂直对齐方式 */
+    align: { type: String, default: "flex-start" },
   },
   setup(props, { slots }) {
     const typeMap = { mini: 4, small: 8, normal: 10, large: 24 };
@@ -18,10 +23,23 @@ export default defineComponent({
       const children = getAllElements(slots.default?.(), true).filter(item => item.type !== Comment);
 
       return (
-        <div class={["bp-space", `bp-space-${props.type}`]} style={`justify-content:${props.justify}`}>
+        <div
+          class={["bp-space", `bp-space-${props.type}`]}
+          style={`justify-content:${props.justify};align-items:${props.align}`}
+        >
           {children.map((child, index) => {
+            const hasSplit = slots.split && index > 0;
+
             return (
               <Fragment key={child.key ?? `item-${index}`}>
+                {hasSplit && (
+                  <div
+                    class="bp-space-item"
+                    style={props.type === "horizontal" ? `margin: 0 ${size}px` : `margin: ${size}px 0`}
+                  >
+                    {slots.split?.()}
+                  </div>
+                )}
                 <div
                   class="bp-space-item"
                   style={props.type === "horizontal" ? `margin: 0 ${size}px` : `margin: ${size}px 0`}
