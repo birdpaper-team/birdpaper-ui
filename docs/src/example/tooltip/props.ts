82c3c6d3944aa79f472d69570f63524721faec4b
Sam <llh@birdpaper.com>
2023-08-30 09:07:45 +0800

    Feat(packages): 2.0.3

--- docs/src/example/tooltip/props.ts
+++ docs/src/example/tooltip/props.ts
@@ -2,6 +2,7 @@ import { PropTableItem } from "../../../components/table-block/src/tableBlock";
 
 const list: PropTableItem[] = [
   { name: "content", remark: "文本提示内容", type: "String", default: "" },
+  { name: "trigger", remark: "触发方式", type: "Enum", optional: ["hover", "click"], default: "hover" },
 ];
 
 export default list;
