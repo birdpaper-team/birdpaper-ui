15a433e23248dc28e7fab7f704d13d1c075d6026
Sam <llh@birdpaper.com>
2023-09-19 15:47:41 +0800

    Feat(packages): popconfirm with trigger

--- packages/components/popconfirm/src/popconfirm.vue
+++ packages/components/popconfirm/src/popconfirm.vue
@@ -1,43 +1,41 @@
 <template>
-  <div :class="name">
-    <Teleport to="body">
-      <Transition name="fade-select" appear>
-        <div ref="innerRef" :class="`${name}-inner`" v-show="show" v-clickOutside="handleCancle">
-          <div :class="`${name}-inner-content`">
-            <span :class="`${name}-icon`">
-              <i v-if="type === 'success'" class="ri-checkbox-circle-fill"></i>
-              <i v-if="type === 'danger'" class="ri-close-circle-fill"></i>
-              <i v-if="type === 'warning'" class="ri-error-warning-fill"></i>
-              <i v-if="type === 'info'" class="ri-information-fill"></i>
-            </span>
-            <span :class="`${name}-text`">
-              {{ content }}
-            </span>
-          </div>
-
-          <div :class="`${name}-inner-footer`">
-            <bp-space size="mini" justify="flex-end">
-              <bp-button @click="handleCancle" size="mini">{{ cancleText }}</bp-button>
-              <bp-button @click="handleOk" :loading="okLoading" size="mini" type="primary" status="primary">
-                {{ okText }}
-              </bp-button>
-            </bp-space>
-          </div>
-        </div>
-      </Transition>
-    </Teleport>
-
-    <div ref="slotRef" :class="`${name}-slot`" @click.stop="handleClick">
+  <bp-trigger v-model:popup-visible="show" :class="name" :popup-offset="10" :position="position">
+    <div :class="`${name}-slot`">
       <slot></slot>
     </div>
-  </div>
+
+    <template #content>
+      <div :class="`${name}-inner`">
+        <div :class="`${name}-inner-content`">
+          <span :class="`${name}-icon`">
+            <i v-if="type === 'success'" class="ri-checkbox-circle-fill"></i>
+            <i v-if="type === 'danger'" class="ri-close-circle-fill"></i>
+            <i v-if="type === 'warning'" class="ri-error-warning-fill"></i>
+            <i v-if="type === 'info'" class="ri-information-fill"></i>
+          </span>
+          <span :class="`${name}-text`">
+            {{ content }}
+          </span>
+        </div>
+
+        <div :class="`${name}-inner-footer`">
+          <bp-space size="mini" justify="flex-end">
+            <bp-button @click="handleCancle" size="mini">{{ cancleText }}</bp-button>
+            <bp-button @click="handleOk" :loading="okLoading" size="mini" type="primary" status="primary">
+              {{ okText }}
+            </bp-button>
+          </bp-space>
+        </div>
+      </div>
+    </template>
+  </bp-trigger>
 </template>
 
 <script lang="ts">
-import { PropType, defineComponent, nextTick, onBeforeUnmount, ref, onMounted } from "vue";
+import { PropType, defineComponent, ref } from "vue";
 import { PopconfirmPosition, PopconfirmType } from "./types";
 import { vClickOutside } from "../../../directives/clickOutside";
-import { off, on, throttle } from "../../../utils/util";
+import BpTrigger from "../../trigger/src/trigger";
 
 export default defineComponent({
   name: "Popconfirm",
@@ -60,39 +58,6 @@ export default defineComponent({
   setup(props, { emit }) {
     const name = "bp-popconfirm";
     const show = ref<boolean>(false);
-    const slotRef = ref();
-    const innerRef = ref();
-
-    const handleClick = () => {
-      handleResize();
-      show.value = true;
-    };
-
-    const handleResize = () => {
-      const slotRect = slotRef.value?.getBoundingClientRect();
-      if (!slotRect) return;
-
-      const position = {
-        top: `top:${slotRect.top - 10 + document.documentElement.scrollTop}px;left:${
-          slotRect.left + slotRect.width / 2
-        }px;transform: translateX(-50%) translateY(-100%);`,
-
-        bottom: `top:${slotRect.top + slotRect.height + 10 + document.documentElement.scrollTop}px;left:${
-          slotRect.left + slotRect.width / 2
-        }px;transform: translateX(-50%);`,
-
-        left: `top:${slotRect.top + slotRect.height / 2 + document.documentElement.scrollTop}px;left:${
-          slotRect.left - 10
-        }px;transform: translateX(-100%) translateY(-50%);`,
-
-        right: `top:${slotRect.top + slotRect.height / 2 + document.documentElement.scrollTop}px;right:${
-          slotRect.right
-        }px;transform: translateX(-100%) translateY(-50%);`,
-      };
-
-      innerRef.value &&
-        innerRef.value.setAttribute("style", `${position[props.position]};display:${show.value ? "block" : "none"}`);
-    };
 
     const handleCancle = () => {
       show.value = false;
@@ -115,23 +80,10 @@ export default defineComponent({
       }
     };
 
-    onMounted(() => {
-      nextTick(() => {
-        on(window, "resize", throttle(handleResize, 100));
-      });
-    });
-
-    onBeforeUnmount(() => {
-      off(window, "resize", handleResize);
-    });
-
     return {
       name,
       show,
-      slotRef,
-      innerRef,
       okLoading,
-      handleClick,
       handleCancle,
       handleOk,
     };
