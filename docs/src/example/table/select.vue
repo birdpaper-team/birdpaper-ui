ff074d9a831a8bb03a224853c85c10b88f6f31a4
sam <llh@birdpaper.com>
2023-11-09 08:59:14 +0800

    Fix(packages): table 空数据全选问题

--- docs/src/example/table/select.vue
+++ docs/src/example/table/select.vue
@@ -1,5 +1,5 @@
 <template>
-  <bp-table row-key="seat" :selection="selection" border :data="list">
+  <bp-table row-key="seat" :selection="selection" border :data="list" v-model:selected-key="keys">
     <template #columns>
       <bp-table-column title="座号" data-index="seat" width="100"></bp-table-column>
       <bp-table-column title="姓名" data-index="name"></bp-table-column>
@@ -10,18 +10,17 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 const list = [
   { seat: "1", name: "李小颖", results: "522", ranking: "4" },
   { seat: "15", name: "杜小浩", results: "462", ranking: "18" },
-  { seat: "18", name: "汤小青", results: "482", ranking: "12" },
-  { seat: "28", name: "田小锋", results: "475", ranking: "15" },
-  { seat: "35", name: "吴小豪", results: "372", ranking: "41" },
-  { seat: "41", name: "吴小可", results: "404", ranking: "33" },
-  { seat: "46", name: "木小亦", results: "409", ranking: "40" },
   { seat: "43", name: "邹小辉", results: "293", ranking: "53" },
 ];
 
 const selection = {
   type: "checkbox",
 };
+
+const keys = ref([]);
 </script>
