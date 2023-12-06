269a2212adaf906c6ed117ba7371ff4f73ac6151
sam <llh@birdpaper.com>
2023-12-06 13:46:01 +0800

    Feat(packages): tag close

--- docs/src/example/tag/events.ts
+++ docs/src/example/tag/events.ts
@@ -1,6 +1,7 @@
 import { EventTableItem } from "../../../components/table-block/src/tableBlock";
 
 const list: EventTableItem[] = [
+  { name: "close", remark: "关闭标签触发", params: "" },
 ];
 
 export default list;
