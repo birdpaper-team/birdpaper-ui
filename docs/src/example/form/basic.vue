090e9658728973a975d86f6581562b2ab9d3724d
sam <llh@birdpaper.com>
2023-12-06 15:20:58 +0800

    Feat(packages): form

--- docs/src/example/form/basic.vue
+++ docs/src/example/form/basic.vue
@@ -1,15 +1,22 @@
 <template>
-  <bp-form ref="formRef" :model="form">
-    <bp-form-item field="name" label="姓名" :rules="[{type:'string', required: true, message: '请输入姓名' }]">
-      <bp-input :style="{ width: '280px' }" v-model="form.name" clearable placeholder="请输入内容"></bp-input>
+  <bp-form ref="formRef" :model="form" :label-col="3" :wrapper-col="21">
+    <bp-form-item field="name" label="姓名" :rules="[{ type: 'string', required: true, message: '请输入姓名' }]">
+      <bp-input :style="{ width: '100%' }" v-model="form.name" clearable placeholder="请输入内容"></bp-input>
     </bp-form-item>
-    <bp-form-item field="intro" label="介绍">
-      <bp-input :style="{ width: '280px' }" :maxlength="500" show-limit v-model="form.intro" clearable placeholder="请输入内容"></bp-input>
+    <bp-form-item field="intro" label="介绍内容">
+      <bp-textarea
+        :style="{ width: '100%' }"
+        :maxlength="500"
+        show-limit
+        v-model="form.intro"
+        clearable
+        placeholder="请输入内容"
+      ></bp-textarea>
     </bp-form-item>
     <bp-form-item>
       <bp-space size="mini">
-        <bp-button type="primary" status="primary" @click="onSubmit">提交</bp-button>
-        <bp-button @click="onReset">重置</bp-button>
+        <bp-button @click="onSubmit">提交</bp-button>
+        <bp-button type="text" @click="onReset">重置</bp-button>
       </bp-space>
     </bp-form-item>
   </bp-form>
