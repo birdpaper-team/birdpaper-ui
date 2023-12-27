d021c06e0c1997adad436f47b0d772adb820a60d
sam <llh@birdpaper.com>
2023-12-27 16:21:37 +0800

    Feat(packages): datepicker

--- packages/components/datePicker/src/components/date-table.vue
+++ packages/components/datePicker/src/components/date-table.vue
@@ -2,13 +2,13 @@
   <div :class="name">
     <div :class="`${name}-header`">
       <div :class="`${name}-header-inner`">
-        <span :class="`${name}-header-inner-year`">2024</span>
-        <span :class="`${name}-header-inner-month`">12月</span>
+        <span :class="`${name}-header-inner-year`">{{ currentYear }}</span>
+        <span :class="`${name}-header-inner-month`">{{ months[currentMonth] }}</span>
       </div>
       <div :class="`${name}-header-option`">
         <IconArrowLeftDoubleFill size="20px" />
-        <IconArrowLeftSLine size="20px" />
-        <IconArrowRightSLine size="20px" />
+        <IconArrowLeftSLine @click="onChangeMonth('prev')" size="20px" />
+        <IconArrowRightSLine @click="onChangeMonth('next')" size="20px" />
         <IconArrowRightDoubleFill size="20px" />
       </div>
     </div>
@@ -16,7 +16,7 @@
       <span :class="`${name}-week-inner`" v-for="v in weeks">{{ v }}</span>
     </div>
     <div :class="`${name}-body`">
-      <div :class="`${name}-body-row`" v-for="row in days">
+      <div :class="`${name}-body-row`" v-for="row in dates">
         <span
           v-for="col in row"
           :class="[`${name}-body-inner`, `day-cell-${col.type}`, { active: currentVal === col.value }]"
@@ -33,14 +33,13 @@
 
 <script lang="ts">
 import { defineComponent, ref, computed, inject } from "vue";
-import dayjs, { Dayjs } from "dayjs";
 import {
   IconArrowLeftSLine,
   IconArrowRightSLine,
   IconArrowLeftDoubleFill,
   IconArrowRightDoubleFill,
 } from "birdpaper-icon";
-import { DayCell, DayType, dateInjectionKey } from "../types";
+import { dateInjectionKey } from "../types";
 import { useDayJs } from "../core";
 
 export default defineComponent({
@@ -51,43 +50,20 @@ export default defineComponent({
     const ctx = ref<any>();
 
     ctx.value = inject(dateInjectionKey);
-    const { getWeeksList } = useDayJs(ctx.value.langs);
+    const { current, currentMonth, currentYear, dates, setDates, changeMonth, weeks, months } = useDayJs(
+      ctx.value.langs
+    );
 
-    const weeks = getWeeksList();
-    const days = ref<DayCell[][]>([[], [], [], [], [], []]);
+    const currentVal = current.value.format(ctx.value.valueFormat);
 
-    const current = dayjs();
-    const currentVal = current.format(ctx.value.valueFormat);
+    setDates(ctx.value.valueFormat);
 
-    const firstDay = current.startOf("month").day();
-    const lastDate = current.endOf("month").date();
-
-    const startDate = computed(() => {
-      return current.startOf("month").subtract(firstDay || 7, "day");
-    });
-
-    const setDays = () => {
-      let sum = 0;
-      for (let row = 0; row < days.value.length; row++) {
-        for (let col = 0; col < 7; col++) {
-          const value = startDate.value.add(sum, "day");
-          const label = value.date().toString();
-
-          let type: DayType = "normal";
-          if (sum < firstDay) {
-            type = "prev";
-          }
-          if (sum - firstDay >= lastDate) {
-            type = "next";
-          }
-
-          days.value[row][col] = { type, value: value.format(ctx.value.valueFormat), label };
-          sum++;
-        }
-      }
+    const onChangeMonth = (type: "prev" | "next") => {
+      let v = currentMonth.value;
+      type === "next" ? v++ : v--;
+      changeMonth(v);
+      setDates(ctx.value.valueFormat);
     };
-
-    setDays();
 
     return {
       ctx,
@@ -95,7 +71,11 @@ export default defineComponent({
       currentVal,
       name,
       weeks,
-      days,
+      months,
+      dates,
+      currentYear,
+      currentMonth,
+      onChangeMonth,
     };
   },
 });
--- packages/components/datePicker/src/core.ts
+++ packages/components/datePicker/src/core.ts
@@ -1,19 +1,62 @@
-import { LangsType } from "./types";
+import { DayCell, DayType, LangsType } from "./types";
 import dayjs from "dayjs";
 import localeData from "dayjs/plugin/localeData";
 import "dayjs/locale/zh-cn";
+import { computed, ref } from "vue";
 
 export const useDayJs = (lang: LangsType) => {
   dayjs.locale(lang);
   dayjs.extend(localeData);
 
-  /**
-   * 获取周列表头数据
-   * @returns string[]
-   */
-  const getWeeksList = () => dayjs.weekdaysMin();
+  const current = ref(dayjs());
+  const currentMonth = computed(() => current.value.month());
+  const currentYear = computed(() => current.value.year());
+  /** 当前月起始周N */
+  const firstDay = computed(() => current.value.startOf("month").day());
+  /** 当前最后一天 */
+  const lastDate = computed(() => current.value.endOf("month").date());
+  /** 当前月视图起始日期 */
+  const startDate = computed(() => {
+    return current.value.startOf("month").subtract(firstDay.value || 7, "day");
+  });
+
+  const weeks = dayjs.weekdaysMin();
+  const months = dayjs.monthsShort();
+
+  const dates = ref<DayCell[][]>([[], [], [], [], [], []]);
+  const setDates = (valueFormat: string) => {
+    let sum = 0;
+    for (let row = 0; row < dates.value.length; row++) {
+      for (let col = 0; col < 7; col++) {
+        const value = startDate.value.add(sum, "day");
+        const label = value.date().toString();
+
+        let type: DayType = "normal";
+        if (sum < firstDay.value) {
+          type = "prev";
+        }
+        if (sum - firstDay.value >= lastDate.value) {
+          type = "next";
+        }
+
+        dates.value[row][col] = { type, value: value.format(valueFormat), label };
+        sum++;
+      }
+    }
+  };
+
+  const changeMonth = (m: number) => {
+    current.value = current.value.month(m);
+  };
 
   return {
-    getWeeksList,
+    current,
+    currentMonth,
+    currentYear,
+    dates,
+    setDates,
+    weeks,
+    months,
+    changeMonth,
   };
 };
