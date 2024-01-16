a9b60de2975a8f48408ad45dd2b7e0c2fa8e60d7
sam <llh@birdpaper.com>
2024-01-16 10:48:44 +0800

    Feat(packages): time select

--- packages/components/timePicker/src/components/time-table.vue
+++ packages/components/timePicker/src/components/time-table.vue
@@ -1,19 +1,30 @@
 <template>
   <div :class="name">
     <div :class="`${name}-body`">
-      <template v-for="items in [hourList, minuteList, secondList]">
-        <RecycleScroller :class="`${name}-time-col`" :items="items" :item-size="32" v-slot="{ item }">
-          <div :class="`${name}-col-cell`">
+      <template v-for="(items, index) in [hourList, minuteList, secondList]">
+        <RecycleScroller
+          listClass="time-col-scroller-list"
+          :ref="el => (typeRefs[index] = el)"
+          :class="`${name}-time-col`"
+          :items="items"
+          :item-size="28"
+          v-slot="{ item }"
+        >
+          <div :class="[`${name}-col-cell`, { active: item === globalValue[index] }]" @click="handleClick(index, item)">
             <span :class="`${name}-col-cell-inner`">{{ item }}</span>
           </div>
         </RecycleScroller>
       </template>
     </div>
+    <div :class="`${name}-footer`">
+      <bp-button size="mini">现在</bp-button>
+      <bp-button disabled size="mini" type="primary" status="primary">确认</bp-button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, inject } from "vue";
+import { defineComponent, ref, inject, Ref } from "vue";
 import { generateArray } from "../../../../utils/util";
 // @ts-ignore
 import { RecycleScroller } from "vue-virtual-scroller";
@@ -30,12 +41,23 @@ export default defineComponent({
     const minuteList = generateArray(60);
     const secondList = generateArray(60);
 
+    const globalValue = ref<string[]>(["", "", ""]);
+
+    const typeRefs = ref([]) as Ref<any[]>;
+    const handleClick = (index: number, item: string) => {
+      typeRefs.value[index].scrollToItem(item);
+      globalValue.value[index] = item;
+    };
+
     return {
       ctx,
       name,
+      typeRefs,
       hourList,
       minuteList,
       secondList,
+      globalValue,
+      handleClick,
     };
   },
 });
