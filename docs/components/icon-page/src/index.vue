dd536ce03c1d51a474bd24e3a6324f46b6186203
sam <llh@birdpaper.com>
2024-03-14 16:56:29 +0800

    Feat(docs): icon-page

--- docs/components/icon-page/src/index.vue
+++ docs/components/icon-page/src/index.vue
@@ -1,56 +1,41 @@
 <template>
   <div class="icon-page">
-    <div class="icon-page-search-inner">
-      <bp-input v-model="searchKey" size="large" placeholder="Search 2598 Icons" clearable></bp-input>
-    </div>
-
-    <div class="icon-page-inner">
-      <template v-for="item in searchList">
-        <div class="icon-item" @click="handleDetail(item)">
-          <component :is="allIcons[`Icon${item}`]" size="20"></component>
-          <p>{{ item }}</p>
-        </div>
-      </template>
+    <div class="icon-page-container">
+      <img class="icon-page-logo" src="https://cos.birdpaper.com/assets/bp%2Bremix.png" />
+      <p align="center" class="icon-page-tag">
+        <a href="https://www.npmjs.com/package/birdpaper-icon" target="_blank">
+          <img class="tag" src="https://img.shields.io/npm/v/birdpaper-icon.svg?style=badge" />
+        </a>
+        <a href="https://npmcharts.com/compare/birdpaper-icon?minimal=true">
+          <img class="tag" src="http://img.shields.io/npm/dm/birdpaper-icon.svg" />
+        </a>
+        <a href="https://icon.birdpaper.design">
+          <img class="tag" src="https://img.shields.io/badge/platform-Vue3-059669.svg" />
+        </a>
+        <a href="https://icon.birdpaper.design">
+          <img class="tag" src="https://img.shields.io/badge/license-MIT-red.svg" />
+        </a>
+      </p>
+      <p class="icon-page-text">
+        图标组件已独立开源为 <a :href="homeUrlMap.bpIcon" target="_blank">Birdpaper Icon</a>，它基于优秀的开源图标库
+        <a :href="homeUrlMap.remix">RemixIcon</a> 实现。
+      </p>
+      <bp-space justify="center">
+        <bp-button type="primary" size="small" status="primary" :icon="IconGithubFill" @click="linkTo('github')"> Github </bp-button>
+        <bp-button :icon="IconNpmjsFill" size="small" @click="linkTo('npm')">npm</bp-button>
+      </bp-space>
     </div>
   </div>
-
-  <icon-modal ref="iconModalRef"></icon-modal>
 </template>
 
 <script setup lang="ts" name="icon-page">
-import * as allIcons from "birdpaper-icon";
-import iconModal from "./components/icon-modal.vue";
-import { ref, computed } from "vue";
+import { IconGithubFill, IconNpmjsFill } from "birdpaper-icon";
 
-const filterName = (str: string) => {
-  if (str) {
-    return str.replace("Icon", "");
-  }
-  return "";
+const homeUrlMap = {
+  bpIcon: "https://icon.birdpaper.design",
+  remix: "https://remixicon.cn",
+  github: "https://github.com/birdpaper-team/birdpaper-icon",
+  npm: "https://www.npmjs.com/package/birdpaper-icon",
 };
-
-const searchKey = ref<string>("");
-const iconNames = ref<string[]>([]);
-
-iconNames.value = [];
-const init = () => {
-  for (const key in allIcons) {
-    if (key !== "default") {
-      iconNames.value.push(filterName(key));
-    }
-  }
-};
-
-const searchList = computed<string[]>(() => {
-  return iconNames.value.filter(item => {
-    return item.toLocaleUpperCase().includes(searchKey.value.toLocaleUpperCase());
-  });
-});
-
-init();
-
-const iconModalRef = ref();
-const handleDetail = (item: string) => {
-  iconModalRef.value.open(item);
-};
+const linkTo = (type: string) => window.open(homeUrlMap[type]);
 </script>
