643cd51c24310ecae96b86d1d807806d21f69a5e
sam <llh@birdpaper.com>
2024-04-11 14:32:22 +0800

    docs update

---
+++ docs/components/home-page/components/stack-block.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="stack-box">
+    <div id="embed"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import sdk from '@stackblitz/sdk';
+import { nextTick, onMounted } from "vue";
+import { useData } from "vitepress";
+
+const init = () => {
+  if (!window.location.href.includes("birdpaper")) return;
+
+  sdk.embedProjectId(
+      'embed',
+      'birdpaper-ui',
+      {
+        // forceEmbedLayout: true,
+        // clickToLoad: true,
+        theme: 'light',
+        openFile: 'src/App.vue',
+        hideNavigation: true,
+        height: 640
+      }
+  );
+}
+onMounted(() => {
+  nextTick(() => init());
+})
+
+</script>
--- docs/components/home-page/index.vue
+++ docs/components/home-page/index.vue
@@ -2,6 +2,7 @@
   <div class="home-page">
     <banner />
     <quick-entry />
+    <stack-block />
     <start-entry />
   </div>
 
@@ -11,6 +12,7 @@
 <script setup lang="ts">
 import banner from "./components/banner.vue";
 import quickEntry from "./components/quick-entry.vue";
+import stackBlock from "./components/stack-block.vue";
 import startEntry from "./components/start-entry.vue";
 import siteFooter from "./components/site-footer.vue";
 </script>
