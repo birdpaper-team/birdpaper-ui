8fc521c05390182a7d6d142f45f7cefa8dbb9228
Sam <llh@birdpaper.com>
2024-07-31 15:29:25 +0800

    :recycle: refactor(docs): test build

--- docs/src/.vitepress/components/home-page.vue
+++ docs/src/.vitepress/components/home-page.vue
@@ -1,8 +1,7 @@
 <template>
-  <div h-full text-center flex select-none>
-    <div ma>
-      <div text-5xl fw100>birdpaper-ui</div>
-      <div op30 text-lg fw300 m1>The instant on-demand Atomic CSS engine.</div>
+  <div class="h-full text-center flex select-none">
+    <div class="ma">
+      <div class="text-5xl fw100">birdpaper-ui</div>
     </div>
   </div>
 </template>
