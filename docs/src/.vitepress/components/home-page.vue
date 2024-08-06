47b241eb5a9b1691ce175aba1bd6d2c702314445
Sam <llh@birdpaper.com>
2024-08-06 19:31:21 +0800

    :lipstick: style(docs): nav config

--- docs/src/.vitepress/components/home-page.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="h-full text-center flex select-none">
-    <div class="ma">
-      <div class="text-5xl fw100">birdpaper-ui</div>
-    </div>
-  </div>
-</template>
--- docs/src/.vitepress/config/locales/zh-CN/nav.ts
+++ docs/src/.vitepress/config/locales/zh-CN/nav.ts
@@ -1,6 +1,22 @@
 export const nav = [
-  { text: "设计", link: "/design/introduction", activeMatch: "/design/" },
-  { text: "指南", link: "/guide/easystart", activeMatch: "/guide/" },
+  {
+    text: "设计", activeMatch: "/design/",
+    items: [
+      { text: "价值观", link: "/design/introduction", activeMatch: "/design/" },
+      { text: "样式指南", link: "/design/introduction", activeMatch: "/design/" },
+      { text: "设计资源", link: "/design/introduction", activeMatch: "/design/" }
+    ]
+  },
+  {
+    text: "开发", activeMatch: "/guide/", items: [
+      { text: "快速上手", link: "/develop/introduction", activeMatch: "/develop/" },
+      { text: "组件目录", link: "/develop/introduction", activeMatch: "/develop/" },
+      { text: "颜色", link: "/develop/introduction", activeMatch: "/develop/" },
+      { text: "定制主题", link: "/develop/introduction", activeMatch: "/develop/" },
+      { text: "贡献指南", link: "/develop/introduction", activeMatch: "/develop/" },
+      { text: "更新日志", link: "/develop/introduction", activeMatch: "/develop/" },
+    ]
+  },
   { text: "组件", link: "/component/grid", activeMatch: "/component/" },
-  { text: "图标", link: "https://icon.birdpaper.design" },
+  { text: "图标", link: "https://icon.birdpaper.design" }
 ];
