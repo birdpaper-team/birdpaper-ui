022611864e052b3f2419e67be24cb95b4c2b4dad
Sam <llh@birdpaper.com>
2024-08-12 10:42:12 +0800

    :sparkles: feat(docs): docs tabs

--- docs/src/.vitepress/components/doc-tabs/src/doc-tabs.vue
+++ docs/src/.vitepress/components/doc-tabs/src/doc-tabs.vue
@@ -1,17 +1,25 @@
 <template>
-  <div>
-    <bp-radio-group type="button" v-model="tab">
-      <bp-radio v-for="v in tabList" :value="v.value">{{ v.label }}</bp-radio>
-    </bp-radio-group>
-
-    <div v-show="tab === 'demo'">
-      <slot name="demo"></slot>
+  <div :class="name">
+    <div class="docs-radio-group">
+      <div
+        :class="['docs-radio', 'select-none', { active: v.value === tab }]"
+        v-for="v in tabList"
+        @click="onSelect(v)"
+      >
+        <span>{{ v.label }}</span>
+      </div>
     </div>
-    <div v-show="tab === 'api'">
-      <slot name="api"></slot>
-    </div>
-    <div v-show="tab === 'guide'">
-      <slot name="guide"></slot>
+    
+    <div :class="`${name}-content`">
+      <div v-show="tab === 'demo'">
+        <slot name="demo"></slot>
+      </div>
+      <div v-show="tab === 'api'">
+        <slot name="api"></slot>
+      </div>
+      <div v-show="tab === 'guide'">
+        <slot name="guide"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -19,10 +27,15 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 
+const name = "doc-tabs";
 const tabList = [
   { label: "示例", value: "demo" },
   { label: "API", value: "api" },
   { label: "指南", value: "guide" },
 ];
 const tab = ref("demo");
+
+const onSelect = (v: { label: string; value: string }) => {
+  tab.value = v.value;
+};
 </script>
