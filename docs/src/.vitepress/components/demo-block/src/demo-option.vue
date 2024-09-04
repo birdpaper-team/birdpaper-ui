aa32c7cc0cf3263b8d2ea8fc5e4146dd78ca1ddb
Sam <llh@birdpaper.com>
2024-09-04 14:55:17 +0800

    :lipstick: style(docs): demo and package

--- docs/src/.vitepress/components/demo-block/src/demo-option.vue
+++ docs/src/.vitepress/components/demo-block/src/demo-option.vue
@@ -2,26 +2,12 @@
   <div :class="['icon-item', { 'icon-item-active': model }]" @click="handleShowCode">
     <IconCodeLine size="13" />
   </div>
-  <!-- <bp-space size="mini" justify="flex-end">
-    <bp-tooltip content="展示代码">
-      <div
-        :class="['icon-item', { active: showCode }]"
-        @click="showCode = !showCode"
-      >
-        <IconCodeLine size="13" />
-      </div>
-    </bp-tooltip>
-    <bp-tooltip content="在 StackBlitz 中调试">
-      <div class="icon-item">
-        <IconFlashlightLine size="13" />
-      </div>
-    </bp-tooltip>
-    <bp-tooltip content="在 Github 上编辑">
-      <div class="icon-item">
-        <IconEditLine size="13" />
-      </div>
-    </bp-tooltip>
-  </bp-space> -->
+  <div class="icon-item">
+    <IconFlashlightLine size="13" />
+  </div>
+  <div class="icon-item">
+    <IconEditLine size="13" />
+  </div>
 </template>
 
 <script setup lang="ts">
