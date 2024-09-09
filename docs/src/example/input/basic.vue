d4d0781af6fbbd71a29543f124b172e36eefbc48
Sam <llh@birdpaper.com>
2024-09-09 16:09:15 +0800

    :sparkles: feat(packages): input optim

--- docs/src/example/input/basic.vue
+++ docs/src/example/input/basic.vue
@@ -1,5 +1,22 @@
 <template>
-  <bp-input v-model="txt" style="width: 220px;" placeholder="请输入内容"></bp-input>
+  <bp-input v-model="txt" clearable size="default" style="width: 220px;margin-bottom: 16px;" placeholder="请输入内容"></bp-input>
+  <bp-input v-model="txt" type="password" clearable size="default" style="width: 220px;margin-bottom: 16px;" placeholder="请输入内容"></bp-input>
+  <bp-input v-model="txt" :maxlength="10" show-limit clearable size="default" style="width: 220px;margin-bottom: 16px;" placeholder="请输入内容">
+    <template #suffix>
+      <!-- RMB -->
+      <IconQrScan2Line  />
+    </template>
+  </bp-input>
+  <bp-input v-model="txt" :maxlength="10" show-limit clearable size="default" style="width: 220px;margin-bottom: 16px;" placeholder="请输入内容">
+  </bp-input>
+  <bp-input v-model="txt" :maxlength="10" show-limit clearable size="default" style="width: 220px;margin-bottom: 16px;" placeholder="请输入内容">
+    <template #perfix>
+      <IconMoneyDollarCircleLine  />
+    </template>
+    <template #suffix>
+      RMB
+    </template>
+  </bp-input>
 </template>
 
 <script setup lang="ts">
--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="cls" @click="handleClick">
-    <div :class="`${clsBlockName}-perfix`" v-if="slots.perfix">
+  <div :class="cls" @click="focus">
+    <div :class="`${clsBlockName}-perfix select-none`" v-if="slots.perfix">
       <slot name="perfix"></slot>
     </div>
     <input
@@ -17,11 +17,13 @@
       @focus="onFocus"
       @blur="onBlur"
       @input="onInput"
+      @keypress="onKeypress"
+      @keyup="onKeyup"
     />
     <div :class="`${clsBlockName}-suffix select-none`" v-if="innerActionIcon || slots.suffix">
       <div :class="`${clsBlockName}-suffix-inner`" v-if="!slots.suffix">
         <component v-if="innerActionIcon && !!model" :is="innerActionIcon" class="action-icon" @click.stop="handleActionIconClick" />
-        <span class="suffix-content">{{ suffixContent }}</span>
+        <span class="suffix-content">{{ innerSuffixContent }}</span>
       </div>
 
       <span class="suffix-content" v-else> <slot name="suffix" /></span>
@@ -41,21 +43,19 @@ defineOptions({ name: "Input" });
 const { clsBlockName } = useNamespace("input");
 
 const model = defineModel<string | number>({
-  required: true,
   default: "",
 });
 const props: InputProps = defineProps(inputProps);
+const emits = defineEmits(["input", "focus", "blur", "keypress", "keyup"]);
 const slots = useSlots();
 
 const cls = computed<string[] | {}[]>(() => [clsBlockName, `${clsBlockName}-${props.size}`, props.disabled && `${clsBlockName}-disabled`]);
-const inpRef = ref<HTMLInputElement>();
+const inpType = computed<InputType>(() => (props.type === "text" ? "text" : isEyeOpen.value ? "password" : "text"));
 
-const handleClick = () => {
-  focus();
-};
-
-// Suffix.
+/** The password text is hide or not. */
 const isEyeOpen = ref<boolean>(true);
+
+/** Inner action icon. */
 const innerActionIcon = computed<Component>(() => {
   if (props.type === "password" && props.showPassword) {
     return !isEyeOpen.value ? IconEyeCloseFill : IconEyeFill;
@@ -67,38 +67,58 @@ const innerActionIcon = computed<Component>(() => {
 
   return null;
 });
-const suffixContent = computed<string | Component>(() => {
+/** Inner suffix content. */
+const innerSuffixContent = computed<string | Component>(() => {
   if (props.maxlength && props.showLimit) {
     return `${String(model.value).length}/${props.maxlength}`;
   }
   return "";
 });
 
+/** Handle action icon click */
 const handleActionIconClick = () => {
-  if (props.type === "text" && props.clearable) return handleClear();
-  if (props.type === "password" && props.showPassword) return handleEyeOpen();
+  if (props.disabled || props.readonly) return;
+
+  if (props.type === "text" && props.clearable) return clear(true);
+  if (props.type === "password" && props.showPassword) return triggerEye();
 };
 
-const inpType = computed<InputType>(() => (props.type === "text" ? "text" : isEyeOpen.value ? "password" : "text"));
-const handleEyeOpen = () => {
+/**
+ * Trigger password text in password type and the `show-password` must be true.
+ */
+const triggerEye = () => {
+  if (!props.showPassword) return;
   isEyeOpen.value = !isEyeOpen.value;
 };
-const handleClear = () => {
+
+/**
+ * Clear input content.
+ * @param autoFocus false
+ */
+const clear = (autoFocus: boolean = false) => {
   model.value = "";
-  nextTick(() => focus());
+  autoFocus && nextTick(() => focus());
 };
 
+const inpRef = ref<HTMLInputElement>();
 const focus = () => inpRef.value?.focus();
 const blur = () => inpRef.value?.blur();
-const onFocus = () => {};
-const onBlur = () => {};
+const onFocus = () => emits("focus");
+const onBlur = () => emits("blur");
+const onKeypress = () => emits("keypress");
+const onKeyup = () => emits("keyup");
 
 const onInput = (e: Event) => {
   model.value = (e.target as HTMLInputElement).value;
+  emits("input");
 };
 
 defineExpose({
   focus,
   blur,
+  triggerEye,
+  clear,
+  model,
+  isEyeOpen,
 });
 </script>
