1a9c5516e26f760cb2aa15fc22d096c304aeac4b
Sam <llh@birdpaper.com>
2024-09-09 13:41:50 +0800

    :sparkles: feat(packages): input core

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,16 +1,40 @@
 <template>
-  <div :class="cls">
-    <div :class="`${name}-perfix`" v-if="slots.perfix">
+  <div :class="cls" @click="handleClick">
+    <div :class="`${clsBlockName}-perfix`" v-if="slots.perfix">
       <slot name="perfix"></slot>
     </div>
-    <input ref="inpRef" :class="`${clsBlockName}-inner`" :spellcheck="false" :name :type :disabled :placeholder :maxlength :value="model" @focus="onFocus" @input="onInput" />
+    <input
+      ref="inpRef"
+      :class="`${clsBlockName}-inner`"
+      :name
+      :disabled
+      :placeholder
+      :maxlength
+      :type="inpType"
+      :value="model"
+      :spellcheck="false"
+      @focus="onFocus"
+      @blur="onBlur"
+      @input="onInput"
+    />
+    <div :class="`${clsBlockName}-suffix select-none`" v-if="innerActionIcon || slots.suffix">
+      <div :class="`${clsBlockName}-suffix-inner`" v-if="!slots.suffix">
+        <component v-if="innerActionIcon && !!model" :is="innerActionIcon" class="action-icon" @click.stop="handleActionIconClick" />
+        <span class="suffix-content">{{ suffixContent }}</span>
+      </div>
+      
+      <span class="suffix-content" v-else> <slot name="suffix" /></span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useNamespace } from "@birdpaper-ui/hooks";
 import { InputProps, inputProps } from "./props";
-import { computed, useSlots, ref } from "vue";
+import { computed, useSlots, ref, nextTick } from "vue";
+import type { Component } from "vue";
+import { IconCloseLine, IconEyeFill, IconEyeCloseFill } from "birdpaper-icon";
+import { InputType } from "./types";
 
 defineOptions({ name: "Input" });
 const { clsBlockName } = useNamespace("input");
@@ -22,15 +46,58 @@ const model = defineModel<string | number>({
 const props: InputProps = defineProps(inputProps);
 const slots = useSlots();
 
-const cls = computed(() => [clsBlockName, `${clsBlockName}-${props.size}`]);
-
+const cls = computed<string[]>(() => [clsBlockName, `${clsBlockName}-${props.size}`]);
 const inpRef = ref<HTMLInputElement>();
 
-const isFocus = ref<boolean>(false);
-const handleFocus = () => inpRef.value?.focus();
+const handleClick = () => {
+  focus();
+};
+
+// Suffix.
+const isEyeOpen = ref<boolean>(true);
+const innerActionIcon = computed<Component>(() => {
+  if (props.type === "password" && props.showPassword) {
+    return !isEyeOpen.value ? IconEyeCloseFill : IconEyeFill;
+  }
+
+  if (props.clearable) {
+    return IconCloseLine;
+  }
+
+  return null;
+});
+const suffixContent = computed<string | Component>(() => {
+  if (props.maxlength && props.showLimit) {
+    return `${String(model.value).length}/${props.maxlength}`;
+  }
+  return "";
+});
+
+const handleActionIconClick = () => {
+  if (props.type === "text" && props.clearable) return handleClear();
+  if (props.type === "password" && props.showPassword) return handleEyeOpen();
+};
+
+const inpType = computed<InputType>(() => (props.type === "text" ? "text" : isEyeOpen.value ? "password" : "text"));
+const handleEyeOpen = () => {
+  isEyeOpen.value = !isEyeOpen.value;
+};
+const handleClear = () => {
+  model.value = "";
+  nextTick(() => focus());
+};
+
+const focus = () => inpRef.value?.focus();
+const blur = () => inpRef.value?.blur();
 const onFocus = () => {};
+const onBlur = () => {};
 
 const onInput = (e: Event) => {
   model.value = (e.target as HTMLInputElement).value;
 };
+
+defineExpose({
+  focus,
+  blur,
+});
 </script>
