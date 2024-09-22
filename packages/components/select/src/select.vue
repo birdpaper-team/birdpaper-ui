cc2ff7a113a0ef8a35979cfec566b30b4f7085be
Sam <llh@birdpaper.com>
2024-09-22 10:10:01 +0800

    :sparkles: feat(packages): select

--- packages/components/select/src/select.vue
+++ packages/components/select/src/select.vue
@@ -8,9 +8,21 @@
     auto-fit-width
     update-at-scroll
   >
-    <bp-input ref="inpRef" :disabled="disabled" v-model="model" readonly :placeholder="placeholder">
+    <bp-input
+      :disabled="disabled"
+      v-model="currentSelect.label"
+      :placeholder="placeholder"
+      readonly
+      @mouseenter="handleMouseEnter"
+      @mouseleave="handleMouseLeave"
+    >
       <template #suffix>
-        <IconArrowDownSLine />
+        <IconCloseLine
+          v-if="!disabled && showClear && currentSelect.label"
+          class="click-icon"
+          @click.stop="handleClear"
+        />
+        <component v-else :is="isFocus ? IconArrowUpSLine : IconArrowDownSLine"></component>
       </template>
     </bp-input>
 
@@ -18,7 +30,7 @@
       <ul :class="`${clsBlockName}-content select-none`">
         <slot v-if="hasOptions"></slot>
 
-        <div v-else>Empty.</div>
+        <div v-else :class="`${clsBlockName}-empty`">Empty.</div>
       </ul>
     </template>
   </bp-trigger>
@@ -30,8 +42,8 @@ import BpTrigger from "@birdpaper-ui/components/trigger/index";
 import { useNamespace } from "@birdpaper-ui/hooks";
 import { SelectProps, selectProps } from "./props";
 import { selectInjectionKey, SelectOption, SelectValue } from "./types";
-import { IconArrowDownSLine } from "birdpaper-icon";
-import { computed, provide, ref, useSlots, VNode, watch } from "vue";
+import { IconArrowDownSLine, IconArrowUpSLine } from "birdpaper-icon";
+import { computed, provide, reactive, ref, useSlots, watchEffect } from "vue";
 import { getAllElements } from "@birdpaper-ui/components/utils/dom";
 
 defineOptions({ name: "Select" });
@@ -62,29 +74,39 @@ const valueMap = computed(() => {
   }
 });
 
-const currentSelect = ref<SelectOption>(new SelectOption());
+const showClear = ref<boolean>(false);
+const handleMouseEnter = () => {
+  if (!props.clearable) return;
+  showClear.value = true;
+};
+const handleMouseLeave = () => {
+  if (!props.clearable) return;
+  showClear.value = false;
+};
+
+const handleClear = () => {
+  currentSelect.value = "";
+  currentSelect.label = "";
+};
+
+const currentSelect = reactive<SelectOption>(new SelectOption());
 provide(selectInjectionKey, {
   modelValue: model.value,
-  currentSelect: currentSelect.value,
+  currentSelect: currentSelect,
   onSelect: (v: SelectValue, payload: SelectOption) => {
-    currentSelect.value.value = v;
-    currentSelect.value.label = payload.label;
+    currentSelect.value = v;
+    currentSelect.label = payload.label;
+    model.value = v;
 
     isFocus.value = false;
   },
 });
 
-const setValue = () => {
-  currentSelect.value.value = model.value;
-  currentSelect.value.label = valueMap.value[currentSelect.value.value];
-};
-
-watch(
-  () => valueMap.value,
-  () => setValue(),
-  {
-    immediate: true,
-    deep: true,
+watchEffect(() => {
+  if (!model.value) {
+    return;
   }
-);
+  currentSelect.value = model.value;
+  currentSelect.label = valueMap.value[model.value] || model.value;
+});
 </script>
