982f450e826e20e787eccc9ef9432756284c584b
Sam <llh@birdpaper.com>
2024-09-26 09:14:58 +0800

    :bug: fix(packages): some fix item

--- packages/components/tooltip/index.ts
+++ packages/components/tooltip/index.ts
@@ -3,7 +3,6 @@ import _tooltip from "./src/tooltip.vue";
 export const Tooltip = _tooltip;
 
 export * from "./src/props";
-// export * from "./src/types";
 
 export type TooltipInstance = InstanceType<typeof Tooltip>;
 export default Tooltip;
